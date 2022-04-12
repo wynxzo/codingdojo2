a86d32f426b3ee36e98cb4a62507e8c8986d824a
Your Name <isidora.poblete.sepulvedad@alumnos.sip.cl>
2022-04-12 10:50:32 -0400

    test1.0

---
+++ ysi.js
@@ -0,0 +1,36 @@
+/*
+fragmento de codigo 1
+*/
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+}
+/*
+variables datos
+     auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+*/
+
+/*
+fragmento de codigo 2
+*/
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
+}
+/*
+variables datos
+produce =   ["manzanas", "naranjas"]
+*/
+/*
+fragmento de codigo 3
+*/
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+/*
+variables datos
+movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+*/
