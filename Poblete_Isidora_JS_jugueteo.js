6cd16453125c60d2625d9f9cbbd1811b4ad2bef1
Your Name <isidora.poblete.sepulvedad@alumnos.sip.cl>
2022-04-21 09:21:52 -0400

    cambiosuwu

---
+++ Poblete_Isidora_JS_jugueteo.js
@@ -0,0 +1,23 @@
+function muestraSiElNiñoPuedeSubirALaMontañaRusa (alturaNiño) {
+  if (alturaNiño >= 52) {
+    console.log ("¡Súbete, chico!")
+}
+else {
+//  alturaniño < 52; {
+    console.log ("lo siento chico, tal vez el proximo año.")
+ // }
+}
+}
+muestraSiElNiñoPuedeSubirALaMontañaRusa(50);
+
+function muestraSiElNiñoPuedeSubirALaMontañaRusa (alturaNiño) {
+  if (alturaNiño >= 56) {
+    console.log ("¡Súbete, chico!")
+}
+else {
+//  alturaniño < 56; {
+    console.log ("¡Súbete, chico!")
+ // }
+}
+}
+muestraSiElNiñoPuedeSubirALaMontañaRusa(56);
